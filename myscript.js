4670a62c7604d9e51c72d32cac8b8b2cd9e3c4e0
colman423 <colman0423@gmail.com>
2018-04-14 00:30:11 +0800

    connect to extension from facebook

--- myscript.js
+++ myscript.js
@@ -1,3 +1,8 @@
-function buttonFunction(){
-    console.log("dick");
+var port = chrome.runtime.connect("obegcjnoangfgddbdkhnogkibphnifgb");
+
+function buttonFunction(e){
+    console.log("clicked!");
+    console.log(e);
+
+    port.postMessage(e);
 }
