ffc2487d105a1e6351237875a61ee42df6c1b974
colman423 <colman0423@gmail.com>
2018-04-14 00:30:46 +0800

    connect listener and ajax post

--- eventPage.js
+++ eventPage.js
@@ -1,16 +1,22 @@
+chrome.runtime.onConnectExternal.addListener(function(port) {
+  port.onMessage.addListener(function(msg) {
+    console.log(msg);
+    $.ajax({
+      method: 'POST',
+      url: "https://fuck.com",
+      data: msg,
+        success: function(a, b, c) {
+          console.log(a);
+          console.log(b);
+          console.log(c);
+        },
+        error:  function(a, b, c) {
+          console.log(a);
+          console.log(b);
+          console.log(c);
+        }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if (request.todo == "showPageAction")
-    {
-        chrome.tabs.query({active:true,currentWindow: true}, function(tabs){
-            chrome.pageAction.show(tabs[0].id);
-        });
-        
-    }
+
+    });
+  });
 });
-
-
-
-
-
-
