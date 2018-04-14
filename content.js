025dde1a2f22666b05f5c3ca13aa15757caffd7b
colman423 <colman0423@gmail.com>
2018-04-14 17:24:16 +0800

    update

--- content.js
+++ content.js
@@ -1,16 +1,24 @@
 var script = document.createElement('script');
+script.onload = function() {
+  setTimeout(function() {
+    let id = chrome.runtime.id;
+    // console.log(id);
+    location.href=`javascript:extensionConnect("${id}"); void 0`;
+  }, 1000);
+}
 script.type = 'text/javascript';
 script.src = chrome.extension.getURL('myscript.js');
 document.getElementsByTagName('head')[0].appendChild(script);
 
-
 $('.mbm').each(function(){
+
   var ele = $(this).find('._5pcp').find('span.fsm.fwn.fcg').find('a').attr('href');
   if(ele)
     $(this).find('._5x46').append('<button id="watch-trailer"  style="margin-top:-5px" class="" onclick="buttonFunction(\''+ele+'\')" ><span class="">fuck</button>');
 });
 
 $('body').on('DOMNodeInserted', function (e) {
+
   var target = e.target; //inserted element;
   if ($(e.target).hasClass("mbm")) {
     var ele = $(target).find('._5pcp').find('span.fsm.fwn.fcg').find('a').attr('href');
--- myscript.js
+++ myscript.js
@@ -1,5 +1,9 @@
-var port = chrome.runtime.connect("obegcjnoangfgddbdkhnogkibphnifgb");
+var port = "";
 
+function extensionConnect(extensionId) {
+  console.log(extensionId);
+  port = chrome.runtime.connect(extensionId);
+}
 function buttonFunction(e){
     console.log("clicked!");
     console.log(e);
