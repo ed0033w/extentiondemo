edf8d6b1d5f8fcc75b112264696c97d44e3424f7
ed <ed0033w@gmail.com>
2018-04-30 19:34:39 +0800

    register page display

--- popup.js
+++ popup.js
@@ -13,6 +13,11 @@ function hello() {
         //console.log(red);
         $( "#login" ).click(function(e) {
             //e.stopPropagation();
+            var loginPage = $('.loginPage').css('display');
+            if(loginPage =='none'){
+                $('.loginPage').show(); 
+                $('.registerPage').hide(); 
+            }
             var name= $("#name").val();
             var password= $("#password").val();
 
@@ -25,13 +30,26 @@ function hello() {
                     password: password
                 },
                 success: function(a, b, c) {
-                console.log(a, b, c);
+                //console.log(a, b, c);
                 },
                 error: function(a, b, c) {
-                console.log(a, b, c);
+                //console.log(a, b, c);
                 }
             });
           });
+          $( "#register" ).click(function(e) {
+            //e.stopPropagation();
+            var registerPage = $('.registerPage').css('display');
+            if(registerPage =='none'){
+                $('.loginPage').hide(); 
+                $('.registerPage').show(); 
+            }
+            
+
+            //alert(name+"   "+password);
+            
+          });
+          
         
     
   });
